d1fefbd3fa656da33c06dbe7aced5b5aefae08b8
Nora Cos <ncos@protonmail.ch>
2024-06-27 02:29:33 -0400

    Project

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("java")
+}
+
+group = "ca.noratastic"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes["Main-Class"] = "ca.noratastic.kapuskasing.Main"
+    }
+}
---
+++ src/main/java/ca/noratastic/kapuskasing/Main.java
@@ -0,0 +1,6 @@
+package ca.noratastic.kapuskasing;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
